70bd7ab3aa90a984c9b3737b27df495d7902d14b
Miloš Dinčev <milosdincev98@gmail.com>
2020-11-09 14:20:02 +0100

    Commit 2 - Dodali smo ListBox pod nazivom ListBoxExerciseResults i povezali je sa bazom

--- Exercises_20-2018/Form1.cs
+++ Exercises_20-2018/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,22 @@ namespace Exercises_20_2018
 {
     public partial class Form1 : Form
     {
+        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=FacultyDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection sqlConnection = new SqlConnection();
+            sqlConnection.ConnectionString = connectionString;
+
+            sqlConnection.Open();
+
+            sqlConnection.Close();
+
         }
 
     }
