4a00e7f2ed761a5ebfdc3d3f167da6cde7443ae6
Miloš Dinčev <milosdincev98@gmail.com>
2020-11-09 14:26:30 +0100

    Commit 3 - Napravljena klasa ExerciseResult koja ima polja Id, StudentName, StudentIndex i Points

---
+++ Exercises_20-2018/ExerciseResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises_20_2018
+{
+    class ExerciseResult
+    {
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public string StudentIndex { get; set; }
+        public int Points { get; set; }
+    }
+}
